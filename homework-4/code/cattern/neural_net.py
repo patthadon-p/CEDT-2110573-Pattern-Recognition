fec4a25617928908c839b6e49bf4b40ecd8af2ac
patthadon-p <patthadon.1807@gmail.com>
2026-02-28 18:09:17 +0700

    Homework 4

--- homework-4/code/cattern/neural_net.py
+++ homework-4/code/cattern/neural_net.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 class TwoLayerNet(object):
   """
@@ -35,15 +34,15 @@ class TwoLayerNet(object):
     - output_size: The number of classes C.
     """
     self.params = {}
-    self.params['W1'] = std * np.random.randn(input_size, hidden_size)
-    self.params['b1'] = np.zeros(hidden_size)
-    self.params['W2'] = std * np.random.randn(hidden_size, output_size)
-    self.params['b2'] = np.zeros(output_size)
+    self.params["W1"] = std * np.random.randn(input_size, hidden_size)
+    self.params["b1"] = np.zeros(hidden_size)
+    self.params["W2"] = std * np.random.randn(hidden_size, output_size)
+    self.params["b2"] = np.zeros(output_size)
+
 
   def loss(self, X, y=None, reg=0.0):
     """
-    Compute the loss and gradients for a two layer fully connected neural
-    network.
+    Compute the loss and gradients for a two layer fully connected neural network.
 
     Inputs:
     - X: Input data of shape (N, D). Each X[i] is a training sample.
@@ -64,66 +63,97 @@ class TwoLayerNet(object):
       with respect to the loss function; has the same keys as self.params.
     """
     # Unpack variables from the params dictionary
-    W1, b1 = self.params['W1'], self.params['b1']
-    W2, b2 = self.params['W2'], self.params['b2']
-    N, D = X.shape
+    W1, b1 = self.params["W1"], self.params["b1"]
+    W2, b2 = self.params["W2"], self.params["b2"]
+    N, _ = X.shape
 
-    # Compute the forward pass
-    scores = None
     #############################################################################
-    # T#5: Perform the forward pass, computing the class scores for the      #
-    # input.                                                                    #
+    # T5: Perform the forward pass, computing the class scores for the input.   #
     # Store the result in the scores variable, which should be an array of      #
     # shape (N, C). Note that this does not include the softmax                 #
     # HINT: This is just a series of matrix multiplication.                     #
     #############################################################################
     
+    # Compute the forward pass
+    X1 = X @ W1 + b1
+    X1 = np.maximum(0, X1)
+    scores = X1 @ W2 + b2
+    
     #############################################################################
-    #                              END OF T#5                                #
+    #                                 END OF T5                                 #
     #############################################################################
     
     # If the targets are not given then jump out, we're done
     if y is None:
       return scores
 
-    # Compute the loss
-    loss = None
     #############################################################################
-    # T#6: Finish the forward pass, and compute the loss. This should include#
+    # T6: Finish the forward pass, and compute the loss. This should include    #
     # both the data loss and L2 regularization for W1 and W2. Store the result  #
     # in the variable loss, which should be a scalar. Use the Softmax           #
     # classifier loss.                                                          #
     #############################################################################
     
+    # Compute the loss
+    scores_shifted  = scores - np.max(scores, axis=1, keepdims=True)
+    scores_softmax  = np.exp(scores_shifted)
+    scores_softmax /= np.sum(scores_softmax, axis=1, keepdims=True)
+    
+    loss  = -np.sum(np.log(scores_softmax[np.arange(N), y]))
+    loss /= N
+    loss += 1/2 * reg * ((W1 * W1).sum() + (W2 * W2).sum())
+    
     #############################################################################
-    #                              END OF T#6                                #
+    #                                 END OF T6                                 #
     #############################################################################
 
-    # Backward pass: compute gradients
-    grads = {}
+
     #############################################################################
-    # T#7: Compute the backward pass, computing derivatives of the weights   #
+    # T7: Compute the backward pass, computing derivatives of the weights       #
     # and biases. Store the results in the grads dictionary. For example,       #
-    # grads['W1'] should store the gradient on W1, and be a matrix of same size #
+    # grads["W1"] should store the gradient on W1, and be a matrix of same size #
     # don't forget about the regularization term                                #
     #############################################################################
+    
+    # Backward Pass: Compute Gradients
+    grads = {}
+    
+    dscores = scores_softmax
+    dscores[np.arange(N), y] -= 1
+    dscores /= N
+    
+    grads["W2"] = X1.T @ dscores + reg * W2
+    grads["b2"] = np.sum(dscores, axis=0)
+    
+    dX1 = dscores @ W2.T
+    dX1[X1 <= 0] = 0
+    
+    grads["W1"] = X.T @ dX1 + reg * W1
+    grads["b1"] = np.sum(dX1, axis=0)
 
     #############################################################################
-    #                              END OF T#7                                #
+    #                                 END OF T7                                 #
     #############################################################################
 
     return loss, grads
 
-  def train(self, X, y, X_val, y_val,
-            learning_rate=1e-3, learning_rate_decay=0.95,
-            reg=5e-6, num_iters=100,
-            batch_size=200, verbose=False):
+
+  def train(
+    self,
+    X, y, X_val, y_val,
+    learning_rate=1e-3,
+    learning_rate_decay=0.95,
+    reg=5e-6,
+    num_iters=100,
+    batch_size=200,
+    verbose=False
+  ):
     """
     Train this neural network using stochastic gradient descent.
 
     Inputs:
     - X: A numpy array of shape (N, D) giving training data.
-    - y: A numpy array f shape (N,) giving training labels; y[i] = c means that
+    - y: A numpy array of shape (N,) giving training labels; y[i] = c means that
       X[i] has label c, where 0 <= c < C.
     - X_val: A numpy array of shape (N_val, D) giving validation data.
     - y_val: A numpy array of shape (N_val,) giving validation labels.
@@ -135,6 +165,7 @@ class TwoLayerNet(object):
     - batch_size: Number of training examples to use per step.
     - verbose: boolean; if true print progress during optimization.
     """
+    
     num_train = X.shape[0]
     iterations_per_epoch = max(num_train / batch_size, 1)
 
@@ -144,17 +175,18 @@ class TwoLayerNet(object):
     val_acc_history = []
 
     for it in range(num_iters):
-      X_batch = None
-      y_batch = None
-
       #########################################################################
-      # T#8: Create a random minibatch of training data and labels, storing#
+      # T8: Create a random minibatch of training data and labels, storing    #
       # them in X_batch and y_batch respectively.                             #
       # You might find np.random.choice() helpful.                            #
       #########################################################################
 
+      random_mask = np.random.choice(num_train, batch_size, replace=True)
+      X_batch = X[random_mask]
+      y_batch = y[random_mask]
+      
       #########################################################################
-      #                             END OF YOUR T#8                        #
+      #                            END OF YOUR T8                             #
       #########################################################################
 
       # Compute loss and gradients using the current minibatch
@@ -162,18 +194,24 @@ class TwoLayerNet(object):
       loss_history.append(loss)
 
       #########################################################################
-      # T#9: Use the gradients in the grads dictionary to update the       #
+      # T9: Use the gradients in the grads dictionary to update the           #
       # parameters of the network (stored in the dictionary self.params)      #
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
 
+      self.params["W1"] -= learning_rate * grads["W1"]
+      self.params["b1"] -= learning_rate * grads["b1"]
+      
+      self.params["W2"] -= learning_rate * grads["W2"]
+      self.params["b2"] -= learning_rate * grads["b2"]
+      
       #########################################################################
-      #                             END OF YOUR T#9                        #
+      #                            END OF YOUR T9                             #
       #########################################################################
 
       if verbose and it % 100 == 0:
-        print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+        print("iteration %d / %d: loss %f" % (it, num_iters, loss))
 
       # Every epoch, check train and val accuracy and decay learning rate.
       if it % iterations_per_epoch == 0:
@@ -183,21 +221,24 @@ class TwoLayerNet(object):
         train_acc_history.append(train_acc)
         val_acc_history.append(val_acc)
 
-        # Decay learning rate
         #######################################################################
-        # T#10: Decay learning rate (exponentially) after each epoch        #
+        # T10: Decay learning rate (exponentially) after each epoch           #
         #######################################################################
 
+        # Decay learning rate
+        learning_rate *= learning_rate_decay
+        
         #######################################################################
-        #                             END OF YOUR T#10                      #
+        #                           END OF YOUR T10                           #
         #######################################################################
         
 
     return {
-      'loss_history': loss_history,
-      'train_acc_history': train_acc_history,
-      'val_acc_history': val_acc_history,
+      "loss_history": loss_history,
+      "train_acc_history": train_acc_history,
+      "val_acc_history": val_acc_history,
     }
+
 
   def predict(self, X):
     """
@@ -214,16 +255,16 @@ class TwoLayerNet(object):
       the elements of X. For all i, y_pred[i] = c means that X[i] is predicted
       to have class c, where 0 <= c < C.
     """
-    y_pred = None
 
     ###########################################################################
-    # T#11: Implement this function; it should be VERY simple!              #
+    # T11: Implement this function; it should be VERY simple!                 #
     ###########################################################################
 
+    scores = np.array(self.loss(X))
+    y_pred = np.argmax(scores, axis=1)
+
     ###########################################################################
-    #                              END OF YOUR T#11                         #
+    #                             END OF YOUR T11                             #
     ###########################################################################
 
     return y_pred
-
-
--- homework-4/code/mnist_data/load_mnist.py
+++ homework-4/code/mnist_data/load_mnist.py
@@ -2,14 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import gzip
-import os
-
 import numpy
-from six.moves import xrange
+import gzip
 
 def _read32(bytestream):
-  dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+  dt = numpy.dtype(numpy.uint32).newbyteorder(">")
   return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
 
 
@@ -22,27 +19,36 @@ def extract_images(f):
   Raises:
     ValueError: If the bytestream does not start with 2051.
   """
-  print('Extracting', f.name)
+  
+  print("Extracting", f.name)
+  
   with gzip.GzipFile(fileobj=f) as bytestream:
     magic = _read32(bytestream)
+    
     if magic != 2051:
-      raise ValueError('Invalid magic number %d in MNIST image file: %s' %
-                       (magic, f.name))
+      raise ValueError("Invalid magic number %d in MNIST image file: %s" % (magic, f.name))
+    
     num_images = _read32(bytestream)
     rows = _read32(bytestream)
     cols = _read32(bytestream)
+    
     buf = bytestream.read(rows * cols * num_images)
+    
     data = numpy.frombuffer(buf, dtype=numpy.uint8)
     data = data.reshape(num_images, rows, cols, 1)
+    
     return data
 
 
 def dense_to_one_hot(labels_dense, num_classes):
   """Convert class labels from scalars to one-hot vectors."""
+  
   num_labels = labels_dense.shape[0]
   index_offset = numpy.arange(num_labels) * num_classes
+  
   labels_one_hot = numpy.zeros((num_labels, num_classes))
   labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  
   return labels_one_hot
 
 
@@ -57,55 +63,62 @@ def extract_labels(f, one_hot=False, num_classes=10):
   Raises:
     ValueError: If the bystream doesn't start with 2049.
   """
-  print('Extracting', f.name)
+  
+  print("Extracting", f.name)
+  
   with gzip.GzipFile(fileobj=f) as bytestream:
     magic = _read32(bytestream)
+    
     if magic != 2049:
-      raise ValueError('Invalid magic number %d in MNIST label file: %s' %
-                       (magic, f.name))
+      raise ValueError("Invalid magic number %d in MNIST label file: %s" % (magic, f.name))
+    
     num_items = _read32(bytestream)
+    
     buf = bytestream.read(num_items)
+    
     labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+    
     if one_hot:
       return dense_to_one_hot(labels, num_classes)
+    
     return labels
 
 
-def read_data_sets(train_dir='mnist_data',
-                   one_hot=False,
-                   reshape=True,
-                   validation_size=5000,
-                   seed=None):
+def read_data_sets(
+  train_dir="mnist_data",
+  one_hot=False,
+  reshape=True,
+  validation_size=5000,
+  seed=None
+):
+  """Reads and parses examples from MNIST data files."""
+  
+  TRAIN_IMAGES = "train-images-idx3-ubyte.gz"
+  TRAIN_LABELS = "train-labels-idx1-ubyte.gz"
+  TEST_IMAGES = "t10k-images-idx3-ubyte.gz"
+  TEST_LABELS = "t10k-labels-idx1-ubyte.gz"
 
+  local_file = train_dir + "/" + TRAIN_IMAGES
 
-  TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
-  TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
-  TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
-  TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
-
-  local_file = train_dir + '/' + TRAIN_IMAGES
-
-  with open(local_file, 'rb') as f:
+  with open(local_file, "rb") as f:
     train_images = extract_images(f)
 
-  local_file = train_dir + '/' + TRAIN_LABELS
-  with open(local_file, 'rb') as f:
+  local_file = train_dir + "/" + TRAIN_LABELS
+  with open(local_file, "rb") as f:
     train_labels = extract_labels(f, one_hot=one_hot)
 
-  local_file = train_dir + '/' + TEST_IMAGES
-  with open(local_file, 'rb') as f:
+  local_file = train_dir + "/" + TEST_IMAGES
+  with open(local_file, "rb") as f:
     test_images = extract_images(f)
 
-  local_file = train_dir + '/' + TEST_LABELS
-  with open(local_file, 'rb') as f:
+  local_file = train_dir + "/" + TEST_LABELS
+  with open(local_file, "rb") as f:
     test_labels = extract_labels(f, one_hot=one_hot)
 
   if not 0 <= validation_size <= len(train_images):
     raise ValueError(
-        'Validation size should be between 0 and {}. Received: {}.'
+        "Validation size should be between 0 and {}. Received: {}."
         .format(len(train_images), validation_size))
-  
-  #return train_images, train_labels, test_images, test_labels
   
   validation_images = train_images[:validation_size]
   validation_labels = train_labels[:validation_size]
